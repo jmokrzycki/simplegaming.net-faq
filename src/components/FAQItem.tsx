57b217f4662b6ff4373e2bf6ba5c2dbc45cc9c1b
jmokrzycki <jarek.mokrzycki@gmail.com>
2024-01-23 18:03:12 +0100

    hash added to link

--- src/components/FAQItem.tsx
+++ src/components/FAQItem.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/FAQItem.scss";
+import { useLocation, useNavigate } from "react-router-dom"; // zakładając, że używasz react-router
 
 interface FAQItemProps {
   title: string;
@@ -8,10 +9,28 @@ interface FAQItemProps {
 
 const FAQItem: React.FC<FAQItemProps> = ({ title, content }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+    if (!isOpen) {
+      navigate(`#${title}`);
+    } else {
+      navigate("#");
+    }
+  };
+
+  useEffect(() => {
+    const currentHash = decodeURIComponent(location.hash.substring(1));
+    if (title === currentHash) {
+      setIsOpen(true);
+    }
+  }, [location.hash, title]);
 
   return (
     <div className="faqItem">
-      <div className={`faq-title ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+      <div className={`faq-title ${isOpen ? "open" : ""}`} onClick={handleToggle}>
         {title}
       </div>
       <div className={`faq-content ${isOpen ? "open" : ""}`} dangerouslySetInnerHTML={{ __html: content }} />
